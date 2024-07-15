47b72d6f783611df51bbb5ebcf26c4abde4564c2
Grant Slender <gslender@gmail.com>
2024-07-15 14:16:34 +1000

    added driver settings, discovery code, pairing filter and check for model isGradientONE and app initial axios/http code

--- app.ts
+++ app.ts
@@ -1,11 +1,50 @@
 'use strict';
 
 import Homey from 'homey';
+import axios, { AxiosInstance } from 'axios';
+import { AirQualityData } from './types'
 
-class AirGradientApp extends Homey.App {
+export default class AirGradientApp extends Homey.App {
+
+
+  private async createHttp(address: string): Promise<AxiosInstance> {
+    try {
+      return axios.create({
+        baseURL: address,
+        headers: {
+          'Accept-Encoding': 'gzip, deflate, br',
+          'Accept-Charset': 'utf-8',
+          Accept: 'application/json',
+          'User-Agent': 'HomeyApp'
+        }
+      });
+    } catch (error) {
+      this.log(JSON.stringify(error));
+      throw new Error(`${error}`);
+    }
+  }
 
   async onInit() {
     this.log('AirGradientApp has been initialized');
+  }
+
+  /**
+   * Returns true if the ipaddress provided is AirGradientOne
+   * by requesting the JSON from 'http://{ipaddress}/measures/current'
+   * and comparing that model === 'I-9PSL'
+   */
+  async isAirGradientOne(address: string): Promise<boolean> {
+
+    try {
+      const http = await this.createHttp(address);
+      const response = await http.get(`/measures/current`);
+      if (response && response.data) {
+        const airQualityData: AirQualityData = response.data;
+        return airQualityData.model === 'I-9PSL';
+      }
+    } catch (e) {
+    }
+    return false;
   }
 
 }
--- drivers/indoor/driver.ts
+++ drivers/indoor/driver.ts
@@ -1,14 +1,37 @@
 import Homey from 'homey';
+import AirGradientApp from '../../app'
 
 class IndoorDriver extends Homey.Driver {
+  app!: AirGradientApp
 
   async onInit() {
     this.log('IndoorDriver has been initialized');
+    this.app = this.homey.app as AirGradientApp;
   }
 
   async onPairListDevices() {
-    return [
-    ];
+    const discoveryStrategy = this.homey.discovery.getStrategy("bond");
+    const discoveryResults = discoveryStrategy.getDiscoveryResults(); const devices = await Promise.all(
+      Object.values(discoveryResults).map(async (result) => {
+        const mdnsResult = result as Homey.DiscoveryResultMDNSSD;
+        const isAirGradientOne = await this.app.isAirGradientOne(mdnsResult.address);
+        if (isAirGradientOne) {
+          return {
+            name: mdnsResult.name,
+            data: {
+              id: mdnsResult.id,
+            },
+            settings: {
+              ipaddress: mdnsResult.address,
+            },
+          };
+        }
+        return null;
+      })
+    );
+
+    const filteredDevices = devices.filter(device => device !== null);
+    return filteredDevices;
   }
 
 }
